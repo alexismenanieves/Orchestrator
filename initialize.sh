98505ed9a31fee3fee6d50018c9c71c670a576ce
Alexis Mena <alexismenanieves@hotmail.com>
2025-04-29 23:28:55 -0500

    Add initialize script: template for a project

---
+++ initialize.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# ==================================================================== #
+# Written by: Manu M. on 2023                                          #
+# Purpose: Generate a basic template for data science projects         #
+# Instructions: Execute using >source initialize.sh                    #
+# ==================================================================== #
+currentPath=$(pwd)
+activatePath="/.venv/bin/activate"
+echo "************** Initialize project template **************"
+python -m venv .venv
+source ${currentPath}${activatePath}
+python -m pip install ipykernel nbformat pytest black[jupyter]
+python -m pip install python-dotenv pandas seaborn scikit-learn 
+python -m pip install pydantic uvicorn reflex requests
+python -m pip install --upgrade pip
+python -m pip freeze > requirements.txt
+echo -e ".venv\n.env\n__pycache__" > .gitignore
+echo "# README" > README.md
+mkdir -p data/{raw,baking,final}
+mkdir notebooks src app api img docs
+touch .env docker-compose.yml main.tf
+echo "******************* Template finished *******************"
